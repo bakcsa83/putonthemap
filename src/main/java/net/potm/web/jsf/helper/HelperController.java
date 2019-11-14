b0ac89cc993d55ea281b25f100040d36acfc4385
Zoltan Bakcsa <zoltan@bakcsa.hu>
2019-11-14 14:10:03 +0100

    add basic html template and a helper controller

---
+++ src/main/java/net/potm/web/jsf/helper/HelperController.java
@@ -0,0 +1,39 @@
+/*
+ *     (C) 2019 by Zoltan Bakcsa (zoltan@bakcsa.hu)
+ *     This file is part of "putonthemap".
+ *
+ *     putonthemap is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     putonthemap is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.potm.web.jsf.helper;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
+import java.util.Locale;
+
+@Model
+public class HelperController {
+
+    private Locale locale;
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+}
