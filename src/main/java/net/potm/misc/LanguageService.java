9ddb711d538347410b466c2a5ebf02e1085e2a68
Zoltan Bakcsa <zoltan@bakcsa.hu>
2019-11-15 15:38:59 +0100

    Make language service more efficient

--- src/main/java/net/potm/misc/LanguageService.java
+++ src/main/java/net/potm/misc/LanguageService.java
@@ -1,56 +1,88 @@
 package net.potm.misc;
 
-import net.potm.business.model.Text;
-import net.potm.business.service.TextService;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import net.potm.persistence.service.TextService;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 @ApplicationScoped
 public class LanguageService {
 
-	private static final String defaultLanguage="en";
-	private Map<String,String>textDict=new HashMap<>(200);
-	
-	@Inject
-	Logger log;
-	
-	@Inject
-	TextService ts;
-	
-	private void reloadMap() {
-		List<Text>textList=ts.getText();
-		textDict.clear();
-		textList.forEach(e->textDict.put(e.getId().getKey()+"_"+e.getId().getLanguage(), e.getText()));
-	}
-	
-	@PostConstruct
-	public void init() {
-		reloadMap();
-		log.info("Language service init. Text map size: "+textDict.size());
-	}
+    private static final String DEFAULT_LANGUAGE = "en";
+    private static final String DEFAULT_STRING="???/???";
+    @Inject
+    Logger log;
+
+    @Inject
+    TextService ts;
+
+    private LoadingCache<CacheKey, String> cache;
+
+    @PostConstruct
+    public void init() {
+        CacheLoader<CacheKey, String> loader;
+        loader = new CacheLoader<>() {
+            @Override
+            public String load(CacheKey key) {
+                try {
+                    return ts.getTextByKey(key.getKey(), key.getLanguage()).getText();
+                } catch (Exception e) {
+                    log.log(Level.WARNING, String.format("Could not fetch text. Key: %s; Language: %s", key.getKey(), key.getLanguage()),e);
+                    return null;
+                }
+            }
+        };
+
+        cache = CacheBuilder.newBuilder()
+                .expireAfterAccess(15, TimeUnit.MINUTES)
+                .build(loader);
+
+        log.info("Language service init done");
+    }
 
 
-	public String getText(String key,String language) {
-		String text=textDict.get(key+"_"+language);
-		if(text==null) {
-			log.warning(String.format("Missing text key '%s' for language '%s'", key,language));
-			reloadMap();
-			text=textDict.get(key+"_"+language);
-			if(text==null) {
-				text=textDict.get(key+"_"+defaultLanguage);
-				if(text==null) {
-					text="???/???";
-				}
+    public String getText(String key, String language) {
+    	String text=null;
+    	try{
+			text = cache.get(new CacheKey(key, language));
+			if (text.equals(DEFAULT_STRING)) {
+				text = cache.get(new CacheKey(key, DEFAULT_LANGUAGE));
 			}
+		}catch (ExecutionException e){
+			log.log(Level.SEVERE,"Could not fetch text",e);
 		}
-		return text;
-	}
+
+        return text != null ? text : DEFAULT_STRING;
+    }
+
+    private static class CacheKey {
+        private String key;
+        private String language;
+
+        public CacheKey(String key, String language) {
+            this.key = key;
+            this.language = language;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getLanguage() {
+            return language;
+        }
+
+        public String toString(){
+        	return String.format("Key: %s Language: %s",key,language);
+		}
+    }
 }
