d462d939d069040f112817eeebfaba9bb87ecf6f
Zoltan Bakcsa <zoltan@bakcsa.hu>
2019-11-16 23:24:11 +0100

    update gitignore

---
+++ src/main/java/net/potm/web/rest/TestRest.java
@@ -0,0 +1,39 @@
+/*
+ *     (C) 2019 by Zoltan Bakcsa (zoltan@bakcsa.hu)
+ *     This file is part of "putonthemap".
+ *
+ *     putonthemap is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     putonthemap is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.potm.web.rest;
+
+import net.potm.persistence.model.Person;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/person")
+public class TestRest {
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Person getPerson(){
+        var p=new Person();
+        p.setFirstName("Zoltan");
+        p.setLastName("Bakcsa");
+        return p;
+    }
+}
