3e3d522f588ac0db67c4581c9538d66ffc4eda8b
Zoltan Bakcsa <zoltan@bakcsa.hu>
2019-11-21 17:55:54 +0100

    adapt update script to another db server

--- database/update-db.sh
+++ database/update-db.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-liquibase --driver=org.postgresql.Driver --classpath=postgresql.jar --url="jdbc:postgresql://192.168.56.210:5432/potm-dev" --changeLogFile=db-v0.1.xml --username=potm_dev --password=$1 update
+liquibase --driver=org.postgresql.Driver --classpath=postgresql.jar --url="jdbc:postgresql://192.168.56.130:5432/potm-dev" --changeLogFile=db-v0.1.xml --username=potm_dev --password=$1 update
